a802f8c3f15eb8f8bdce6255e0d242f0924cc40e
AzisSyam <abdazissyam99@gmail.com>
2024-02-07 22:03:00 +0800

    Add project to github repo

---
+++ app/src/main/java/com/pkm/byebyestress/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pkm.byebyestress.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pkm.byebyestress.R
+
+class MainActivity : AppCompatActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
+   }
+}
---
+++ app/src/main/java/com/pkm/byebyestress/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.pkm.byebyestress.ui.splashscreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pkm.byebyestress.R
+
+class SplashScreen : AppCompatActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_splash_screen)
+   }
+}
